e1ca9e8a9de8ef05977f6d2dc80f528821858ad0
vat99 <rrty65899@gmail.com>
2024-12-09 17:37:38 -0500

    remove bad solution

--- day09/solution.py
+++ day09/solution.py
@@ -56,61 +56,6 @@ class Solution:
                 break
         #print(list(enumerate(full_output)))
         return reduce(lambda x, y: x + (y[0]*y[1]) if y[1] != '.' else x, enumerate(full_output), 0)
-    
-    def part02_brute_force(self, fname) -> int:
-        line = self.read_input(fname)
-        #print(len(line))
-        full_output = []
-        file_block_locs = []
-        empty_block_locs = []
-        for i, block_size in enumerate(line):
-            #print(i, block_size)
-            if i % 2 == 0:
-                #full_output.extend([i//2 for _ in range(block_size)])
-                if block_size > 0:
-                    start_interval = len(full_output)
-                    end_interval = start_interval+block_size-1
-                    file_block_locs.append((i//2, (end_interval, start_interval)))
-                for _ in range(block_size):
-                    full_output.append(i//2)
-            else:
-                #full_output.extend(['.' for _ in range(block_size)])
-                if block_size > 0:
-                    start_interval = len(full_output)
-                    end_interval = start_interval+block_size-1
-                    empty_block_locs.append((start_interval, end_interval))
-                for _ in range(block_size):
-                    full_output.append('.')
-            #print(full_output, len(full_output))
-        
-        #print(file_block_locs[::-1])
-        #print(empty_block_locs)
-
-        #print("Swapping")
-        
-        for file_id, (file_end, file_start) in file_block_locs[::-1]:
-            #print(f"file_id: {file_id}, ({file_end}, {file_start})")
-            for j, (empty_start, empty_end) in enumerate(empty_block_locs):
-                #print(f"index: {j}, ({empty_start}, {empty_end})")
-                if empty_end < file_start:
-                    if empty_end-empty_start > file_end-file_start:
-                        for x, y in zip(range(file_end, file_start-1, -1), range(empty_start, empty_start+(file_end-file_start+1)+1)):
-                            #print(y, x)
-                            full_output[y] = full_output[x]
-                            full_output[x] = "."
-                        empty_block_locs[j] = (empty_end-(file_end-file_start)+1, empty_end)
-                        break
-                    elif empty_end-empty_start == file_end-file_start:
-                        for x, y in zip(range(file_end, file_start-1, -1), range(empty_start, empty_start+(file_end-file_start+1)+1)):
-                            #print(y, x)
-                            full_output[y] = full_output[x]
-                            full_output[x] = "."
-                        empty_block_locs.pop(j)
-                        break
-            #print(full_output)
-            #print(file_block_locs)
-            #print(empty_block_locs)
-        return reduce(lambda x, y: x + (y[0]*y[1]) if y[1] != '.' else x, enumerate(full_output), 0)
 
     def part02(self, fname) -> int:
         line = self.read_input(fname)
